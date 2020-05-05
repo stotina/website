260403ab218c47e46aebab5653a4a3cd05bd346d
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-05-05 19:33:28 +0300

    header styling

--- src/components/elements/HeaderElement.vue
+++ src/components/elements/HeaderElement.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="header">
     <nav class="navbar navbar-expand navbar-fixed-top">
-      <a class="navbar-brand" v-on:click="navigate('/')">
-        <span class="site-title">STOTINA</span>
+      <a class="navbar-brand pointer" v-on:click="navigate('/')">
+        <span class="site-title">
+          <img id="site-title-logo" src="../../assets/logo-small.png" />
+          STOTINA
+        </span>
       </a>
 
       <ul class="nav navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" v-on:click="navigate('/')">
+          <a class="nav-link pointer" v-on:click="navigate('/')">
             <i class="fas fa-home"></i> Home
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" v-on:click="navigate('/apps')">Applications</a>
+          <a class="nav-link pointer" v-on:click="navigate('/apps')">Applications</a>
         </li>
       </ul>
     </nav>
@@ -41,6 +44,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.header {
+  border-bottom: 1px solid #96969636;
+}
 .nav-item.active {
   text-decoration: underline;
 }
@@ -48,8 +54,20 @@ export default {
   color: #2a9fd6;
 }
 .site-title {
-  font-size: 1.2em;
+  font-size: 2rem;
+  display: inline-block;
+  padding: 2px 0;
   font-weight: bold;
   font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+}
+#site-title-logo {
+  height: 2rem;
+  margin-left: 0.5rem;
+  margin-right: 0.3rem;
+  border: 2px solid #96969636;
+  border-radius: 2rem;
+}
+.pointer {
+  cursor: pointer;
 }
 </style>
