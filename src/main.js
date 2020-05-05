391b605af5bc2aaaf1c4cbc3113dc7a55abb80e5
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-05-05 13:35:17 +0300

    paging

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import AppNewsPage from "./components/AppNewsPage.vue";
+import AppsPage from "./components/AppsPage.vue";
+import HomePage from "./components/HomePage.vue";
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: HomePage },
+    { path: '/apps', component: AppsPage },
+    { path: '/apps/news', component: AppNewsPage },
+  ]
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
