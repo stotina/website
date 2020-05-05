2d42aa091d89016833419f757e88f27b7df701ed
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-05-05 22:43:40 +0300

    interactive logo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,21 +1,36 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <div class="interactive-logo-container">
+      <InteractiveLogo></InteractiveLogo>
+    </div>
+    <div class="statement">We aim to do better.</div>
   </div>
 </template>
 
 <script>
+import InteractiveLogo from "./elements/InteractiveLogo";
+
 export default {
   name: "HomePage",
   data() {
-    return {
-      msg: "Home Page"
-    };
+    return {};
   },
-  props: {}
+  props: {},
+  components: {
+    InteractiveLogo
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.interactive-logo-container {
+  margin-top: 1rem;
+}
+.statement {
+  margin-top: 3rem;
+  font-size: 2rem;
+  font-style: italic;
+  text-align: center;
+}
 </style>
---
+++ src/components/elements/InteractiveLogo.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="interactive-logo">
+    <div id="logoBox">
+      <div id="logoFrame" class="absolutePosition"></div>
+      <div class="absolutePosition">
+        <img id="logoImage" src="../../assets/logo.png" />
+      </div>
+      <div id="logoFilter"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+
+export default {
+  name: "InteractiveLogo",
+  data() {
+    return {};
+  },
+  props: {},
+  computed: {},
+  methods: {},
+  created() {
+    document.addEventListener("mousemove", e => {
+      var logoBox = $(window.logoBox);
+      var center = [
+        logoBox.offset().left + logoBox.width() / 2,
+        logoBox.offset().top + logoBox.height() / 2
+      ];
+      var angle =
+        Math.atan2(e.pageX - center[0], -(e.pageY - center[1])) *
+        (180 / Math.PI);
+      angle = Math.floor(angle - 180);
+
+      $(window.logoFrame).css(
+        "background-image",
+        `linear-gradient(${angle}deg, #2a9fd6, #06060666, #060606)`
+      );
+      $(window.logoFilter).css(
+        "background-image",
+        `linear-gradient(${angle}deg, transparent, #06060622, #060606)`
+      );
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#logoBox {
+  display: block;
+  margin: auto;
+  text-align: center;
+  width: 40vw;
+  height: 40vw;
+  border-radius: 40vw;
+}
+#logoBox img {
+  width: 40vw;
+  height: 40vw;
+  border-radius: 40vw;
+}
+.absolutePosition {
+  position: absolute;
+}
+#logoFrame,
+#logoFilter {
+  width: 40vw;
+  height: 40vw;
+  border-radius: 40vw;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+#logoFrame {
+  background-image: linear-gradient(180deg, #2a9fd6, #06060666, #060606);
+}
+#logoFilter {
+  position: relative;
+  background-image: linear-gradient(180deg, transparent, #06060666, #060606);
+}
+#logoFilter:hover {
+  opacity: 0;
+  transition: 1s ease-in;
+}
+</style>
