458913f1398d8a10967175744e93751b02b4aef1
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-05-16 17:53:07 +0300

    simplified 3d phones

--- src/components/AppNewsPage.vue
+++ src/components/AppNewsPage.vue
@@ -1,41 +1,25 @@
 <template>
-  <div>
+  <div class="appPage">
     <div class="phones">
-      <div class="phones3d">
-        <div class="phones3d-1">
-          <Phone3DElement
-            :frameColor="'#535353'"
-            :showControls="false"
-            :positionRight="true"
-            :positionSide="150"
-            :positionTop="80"
-            :imagePath="this.screenshot"
-            :y="-15 + this.x * 30"
-            :x="10"
-            :s="1.1"
-          ></Phone3DElement>
-        </div>
-        <div class="phones3d-2">
-          <Phone3DElement
-            :frameColor="'white'"
-            :showControls="false"
-            :positionRight="true"
-            :positionSide="300"
-            :positionTop="120"
-            :imagePath="this.screenshot"
-            :y="-15 + this.x * 30"
-            :x="10"
-            :s="1"
-          ></Phone3DElement>
-        </div>
-      </div>
-      <div class="phones2d">
-        <PhoneElement
-          id="phoneFrame2d"
-          :width="'300px'"
-          :borderColor="'gray'"
+      <div class="phone phone1">
+        <Phone3DElement
+          :frameColor="'#535353'"
+          :showControls="true"
           :imagePath="this.screenshot"
-        ></PhoneElement>
+          :y="-15"
+          :x="10"
+          :s="1.1"
+        ></Phone3DElement>
+      </div>
+      <div class="phone phone2">
+        <Phone3DElement
+          :frameColor="'white'"
+          :showControls="true"
+          :imagePath="this.screenshot"
+          :y="-15"
+          :x="10"
+          :s="1"
+        ></Phone3DElement>
       </div>
     </div>
 
@@ -63,30 +47,19 @@
 
 <script>
 import Phone3DElement from "./elements/Phone3DElement";
-import PhoneElement from "./elements/PhoneElement";
 
 export default {
   name: "AppNewsPage",
   data() {
     return {
-      x: 0.5,
-      w: document.body.clientWidth,
       screenshot: "/images/screenshots/idle.jpg",
     };
   },
   computed: {},
-  created() {
-    document.addEventListener("mousemove", this.onMouseUpdate, false);
-  },
-  methods: {
-    onMouseUpdate(e) {
-      this.w = document.body.clientWidth;
-      this.x = e.pageX / this.w;
-    },
-  },
+  created() {},
+  methods: {},
   components: {
     Phone3DElement,
-    PhoneElement,
   },
   props: {},
 };
@@ -94,12 +67,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.phones2d {
-  position: absolute;
-  top: 6vw;
-  right: 8vw;
-  transform: rotate(5deg);
+.appPage {
+  display: flex;
+  flex-direction: row-reverse;
 }
+.appPage > * {
+  border: 2px solid pink;
+  width: 49vw;
+  display: block;
+  margin: auto;
+}
+
 .content {
   padding-bottom: 120px;
   width: 50%;
@@ -120,28 +98,12 @@ export default {
   max-width: 70%;
 }
 
-@media only screen and (max-width: 1300px) {
-  .phones3d {
-    display: none;
-  }
-}
-@media only screen and (min-width: 1300px) {
-  .phones2d {
-    display: none;
-  }
-}
-@media only screen and (max-width: 850px) {
-  .phones3d {
-    display: none;
-  }
-  .phones2d {
+@media only screen and (max-width: 700px) {
+  .appPage {
     display: block;
-    position: initial;
-    padding: 50px calc(50% - 150px);
   }
-  .content {
-    width: 100%;
-    padding-top: 0;
+  .appPage > * {
+    width: 99vw;
   }
 }
 </style>
--- src/components/elements/Phone3DElement.vue
+++ src/components/elements/Phone3DElement.vue
@@ -1,7 +1,7 @@
 <!-- Component A -->
 <template>
   <div class="wrapper">
-    <div id="controls" style="padding:1rem;" v-if="this.showControls">
+    <div class="controls" v-if="this.showControls">
       <div style="font-size:2em;">Controls for 3D element</div>
       <span>X:</span>
       <input type="range" min="-180" max="180" step="0.1" v-model="x" /> -
@@ -26,31 +26,30 @@
       <input type="number" min="0" max="500" step="10" v-model="positionTop" />
       <br />
       <span>side:</span>
-      <input type="range" min="0" max="500" step="10" v-model="positionSide" /> -
+      <input type="range" min="0" max="500" step="10" v-model="positionSide" />
+      -
       <input type="number" min="0" max="500" step="10" v-model="positionSide" />
     </div>
 
-    <div class="phoneWrapper" v-bind:style="this.wrapperPositionCss">
-      <div class="phoneThickness" v-bind:style="this.getThicknessCSS(1, 6)"></div>
-      <div class="phoneThickness" v-bind:style="this.getThicknessCSS(2, 6)"></div>
-      <div class="phoneThickness" v-bind:style="this.getThicknessCSS(3, 6)"></div>
-      <div class="phoneThickness" v-bind:style="this.getThicknessCSS(4, 6)"></div>
-      <div class="phoneThickness" v-bind:style="this.getThicknessCSS(5, 6)"></div>
-      <div class="phoneThickness" v-bind:style="this.getThicknessCSS(6, 6)"></div>
-      <div
-        class="phoneFrame"
-        v-bind:style=" `border-color:${this.frameColor};${this.getRotationsCSS()};`"
+    <div class="phoneWrapper">
+      <PhoneElement
+        class="phone"
+        :width="'300px'"
+        :borderColor="this.frameColor"
+        :imagePath="this.imagePath"
+        :style="this.getRotationsCSS()"
       >
-        <img :src="this.imagePath" />
-      </div>
+      </PhoneElement>
     </div>
   </div>
 </template>
 
 <script>
+import PhoneElement from "./PhoneElement";
+
 export default {
   name: "Phone3DElement",
-  components: {},
+  components: { PhoneElement },
   data() {
     return {};
   },
@@ -58,72 +57,49 @@ export default {
     getRotationsCSS() {
       return `transform: scale(${this.s}) rotateX(${this.x}deg) rotateY(${this.y}deg) rotateZ(${this.z}deg)`;
     },
-    getThicknessCSS(index, ofTotal) {
-      var translateCSS =
-        `translateX(${-(this.y / ofTotal) * index}px) ` +
-        `translateY(${(this.x / ofTotal) * index}px) ` +
-        `translateZ(${(-0.9 / ofTotal) * index}vw)`;
-      return this.getRotationsCSS() + " " + translateCSS + ";";
-    }
   },
-  computed: {
-    wrapperPositionCss() {
-      return (
-        `position: absolute; ` +
-        `top: ${this.positionTop}px; ` +
-        `${this.positionRight ? "right" : "left"}: ${this.positionSide}px;`
-      );
-    }
-  },
+  computed: {},
   props: {
-    positionRight: {
-      type: Boolean,
-      default: true
-    },
-    positionSide: {
-      type: Number,
-      default: 150
-    },
-    positionTop: {
-      type: Number,
-      default: 100
-    },
     showControls: {
       type: Boolean,
-      default: false
+      default: false,
     },
     x: {
       type: Number,
-      default: 10
+      default: 10,
     },
     y: {
       type: Number,
-      default: -20
+      default: -20,
     },
     z: {
       type: Number,
-      default: 0
+      default: 0,
     },
     s: {
       type: Number,
-      default: 1.1
+      default: 1.1,
     },
     frameColor: {
       type: String,
-      default: "#afafaf"
+      default: "#afafaf",
     },
     imagePath: {
       type: String,
-      default: "/images/screenshots/idle.jpg"
-    }
-  }
+      default: "/images/screenshots/idle.jpg",
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.controls {
+  padding: 1rem;
+  margin: 1rem;
+  border: 1px dotted pink;
+}
 .wrapper {
-  overflow: hidden;
   width: 100%;
   display: inline-block;
   width: auto;
@@ -133,31 +109,5 @@ export default {
   height: 40vw;
   perspective: 500px;
   transform-style: preserve-3d;
-}
-.phoneFrame {
-  display: block;
-  width: 100%;
-  height: 100%;
-  border-style: solid;
-  border-color: white;
-  border-radius: 0.75vw;
-  border-width: 0.75vw;
-  border-top-width: 3vw;
-  border-bottom-width: 1.5vw;
-  position: absolute;
-}
-.phoneFrame img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  border: 1px solid black;
-}
-.phoneThickness {
-  position: absolute;
-  border-radius: 15px;
-  display: block;
-  width: 100%;
-  height: 100%;
-  background-color: #7a7181;
 }
 </style>
