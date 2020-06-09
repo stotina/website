058c3df8bb085d0b87617039ef7f1ff3d20b5ffa
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-06-09 23:04:57 +0300

    FAQ page template

--- src/components/FaqPage.vue
+++ src/components/FaqPage.vue
@@ -1,22 +1,50 @@
 <template>
   <div>
     <div class="faq">
-      <h3>Under Construction...</h3>
+      <h3>FAQ</h3>
+
+      <br />
+      <FaqItem v-for="q in faqData" :key="q" :question="q.q" :answer="q.a">
+      </FaqItem>
+      <br />
     </div>
   </div>
 </template>
 
 <script>
+import FaqItem from "./elements/FaqItem";
+
 export default {
   name: "FaqPage",
   data() {
-    return {};
+    return {
+      faqData: [
+        {
+          q: "Question 1",
+          a: "Answer 1",
+        },
+        {
+          q: "Question 2",
+          a: "Answer 2",
+        },
+        {
+          q: "Question 3",
+          a: "Answer 3",
+        },
+      ],
+    };
   },
   props: {},
-  components: {}
+  components: { FaqItem },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.faq {
+  margin: auto;
+  text-align: center;
+  width: 90%;
+  max-width: 600px;
+}
 </style>
