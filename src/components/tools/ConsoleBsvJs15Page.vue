ef0430d1d5433018523c968318918059bd841fb2
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2022-01-12 22:45:44 +0200

    bsv 1 & 2 console tools

--- src/components/tools/ConsoleBsvJs15Page.vue
+++ src/components/tools/ConsoleBsvJs15Page.vue
@@ -1,22 +1,49 @@
 <template>
   <div>
-    <h1 class="text-center">ConsoleBsvJs15 Not Implemented</h1>
-    <!-- // TODO: Implement this tool -->
+    <h1 class="text-center">CONSOLE: BSV 1.5.6</h1>
+    <div class="console-wrapper">
+      <div id="console-area"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import * as htmlConsole from "../../assets/js/html-console";
+import bsv from "../../assets/js/bsb.1.5.6/bsv.min";
+import bsvMnemonic from "../../assets/js/bsb.1.5.6/bsv-mnemonic.min";
+import bsvMessage from "../../assets/js/bsb.1.5.6/bsv-message.min";
+import bsvEcies from "../../assets/js/bsb.1.5.6/bsv-ecies.min";
+
+window.bsv = bsv
+window.bsvMnemonic = bsvMnemonic
+window.bsvMessage = bsvMessage
+window.bsvEcies = bsvEcies
 
 export default {
   name: "BSVJS-1.5.6",
   data() {
     return {};
   },
+  mounted: () => {
+    htmlConsole.create(document.getElementById("console-area"), {
+      height: "30em",
+    });
+    console.warn(`*** BSV ${bsv.version} loaded as variable 'bsv' ***`);
+    console.log("------------");
+    console.info("Some examples:")
+    console.info("bsv.Transaction.fromHex(hexString)")
+    console.info("bsv.PrivateKey.fromRandom().toAddress()")
+    console.info("bsv.Mnemonic.fromRandom().toString()")
+    console.info("bsv.Script.fromHex('76a9145a608ac563f2f15594dcc070c6d9e1fa14db43a988ac')")
+  },
   props: {},
-  components: { },
+  components: {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.console-wrapper {
+  padding: 20px;
+}
 </style>
--- src/components/tools/ConsoleBsvJs20Page.vue
+++ src/components/tools/ConsoleBsvJs20Page.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
-    <h1 class="text-center">ConsoleBsvJs20 Not Implemented</h1>
-    <!-- // TODO: Implement this tool -->
+    <h1 class="text-center">CONSOLE: BSV 2.0.10</h1>
+    <div class="console-wrapper">
+      <div id="console-area"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import * as htmlConsole from "../../assets/js/html-console";
+import bsvjs from "../../assets/js/bsv.2.0.10/bsv.bundle"
+
+window.bsvjs = bsvjs
 
 export default {
   name: "BSVJS-2.0.10",
   data() {
     return {};
   },
+  mounted: () => {
+    htmlConsole.create(document.getElementById("console-area"), {
+      height: "30em",
+    });
+    console.warn(`*** BSV ${bsvjs.version} loaded as variable 'bsvjs' ***`);
+    console.log("------------");
+    console.info("Some examples:")
+    console.info("bsvjs.Tx.fromHex(hexString)")
+    console.info("bsvjs.Address.fromPrivKey(bsvjs.PrivKey.fromRandom())")
+    console.info("bsvjs.Bip39.fromRandom()")
+    console.info("bsvjs.Script.fromHex('76a9145a608ac563f2f15594dcc070c6d9e1fa14db43a988ac')")
+  },
   props: {},
-  components: { },
+  components: {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.console-wrapper {
+  padding: 20px;
+}
 </style>
