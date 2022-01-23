e8d8a5fcc9854c694a2c7c8550e7a7cd2178155d
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2022-01-23 16:09:04 +0200

    broadcast fix

--- src/components/tools/txdemo/bsvBroadcast.js
+++ src/components/tools/txdemo/bsvBroadcast.js
@@ -1,11 +1,11 @@
-import { broadcastTx } from "../../../assets/js/whatsOnChain.js";
+import { broadcastTx as broadcastTxWoC } from "../../../assets/js/whatsOnChain.js";
 import { getAddress, getNetwork } from "./bsvUtxo.js";
 
-export async function broadcast___Tx(txhex, network = undefined) {
+export async function broadcastTx(txhex, network = undefined) {
   network = network?.toString() || getNetwork()?.toString();
 
   try {
-    return await broadcastTx(txhex, network);
+    return await broadcastTxWoC(txhex, network);
   } catch (error) {
     error.message += "\n Extra Information: " +
         JSON.stringify({ network, address: getAddress().toString(), tx: txhex })
