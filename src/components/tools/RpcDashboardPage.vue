f5f9dd98305d1278d48301bc7d9065728a4eff9a
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2022-02-14 17:08:46 +0200

    rpc improvements

--- src/components/tools/RpcDashboardPage.vue
+++ src/components/tools/RpcDashboardPage.vue
@@ -1,78 +1,105 @@
 <template>
   <div>
     <h1 class="text-center">RPC Dashboard</h1>
+    <div class="row">
+      <div class="rpcSettings col-md-8 row m-auto">
+        <input
+          class="col-md-4 col-sm-4"
+          type="text"
+          v-model="RPC_URL"
+          placeholder="RPC_URL"
+        />
+        <input
+          class="col-md-4 col-sm-4"
+          type="text"
+          v-model="RPC_USERNAME"
+          placeholder="RPC_USERNAME"
+        />
+        <input
+          class="col-md-4 col-sm-4"
+          type="password"
+          v-model="RPC_PASSWORD"
+          placeholder="RPC_PASSWORD"
+        />
+        <div class="col-md-12 text-center">
+          {{
+            RPC_FULL_URL.replace(
+              RPC_USERNAME + ":" + RPC_PASSWORD,
+              RPC_USERNAME + ":" + new Array(RPC_PASSWORD.length + 1).join("*")
+            )
+          }}
+        </div>
+      </div>
+    </div>
     <div class="config"></div>
     <div id="actions" class="row">
-      <div class="col-lg-4 col-md-12 getTx action-item"> 
+      <div class="col-lg-4 col-md-12 getTx action-item">
         <h2>Get Tx</h2>
-        <div class="action-content" >
+        <div class="action-content">
           AAAAAAA
         </div>
-        
       </div>
-      <div class="col-lg-4 col-md-12 mineBlock action-item"> 
+      <div class="col-lg-4 col-md-12 mineBlock action-item">
         <h2>Mine Block</h2>
-        <div class="action-content" >
+        <div class="action-content">
           AAAAAAA
         </div>
-        
       </div>
-      <div class="col-lg-4 col-md-12 submitTx action-item"> 
+      <div class="col-lg-4 col-md-12 submitTx action-item">
         <h2>Submit Tx</h2>
-        <div class="action-content" >
+        <div class="action-content">
           AAAAAAA
         </div>
-        
       </div>
     </div>
     <div class="accordion" id="infoOptions">
       <div class="accordion-item">
-        <h3 class="accordion-header" id="info_title_blockchain">
+        <h3 class="accordion-header" id="info_title_blockchain" v-on:click="expandedElement='blockchain'">
           <div class="accordion-button collapsed">
             Blockchain info
           </div>
         </h3>
-        <div class="accordion-collapse collapse show">
+        <div v-bind:class="{ 'accordion-collapse':true, 'collapse':true, show: expandedElement === 'blockchain' }">
           AAAAAAAAAA
         </div>
       </div>
       <div class="accordion-item">
-        <h3 class="accordion-header" id="info_title_mempool">
+        <h3 class="accordion-header" id="info_title_mempool" v-on:click="expandedElement='mempool'">
           <div class="accordion-button collapsed">
             Mempool info
           </div>
         </h3>
-        <div class="accordion-collapse collapse">
+        <div v-bind:class="{ 'accordion-collapse':true, 'collapse':true, show: expandedElement === 'mempool' }">
           AAAAAAAAAA
         </div>
       </div>
       <div class="accordion-item">
-        <h3 class="accordion-header" id="info_title_rpc">
+        <h3 class="accordion-header" id="info_title_rpc" v-on:click="expandedElement='rpc'">
           <div class="accordion-button collapsed">
             RPC info
           </div>
         </h3>
-        <div class="accordion-collapse collapse">
+        <div v-bind:class="{ 'accordion-collapse':true, 'collapse':true, show: expandedElement === 'rpc' }">
           AAAAAAAAAA
         </div>
       </div>
       <div class="accordion-item">
-        <h3 class="accordion-header" id="info_title_mining">
+        <h3 class="accordion-header" id="info_title_mining" v-on:click="expandedElement='mining'">
           <div class="accordion-button collapsed">
             Mining info
           </div>
         </h3>
-        <div class="accordion-collapse collapse">
+        <div v-bind:class="{ 'accordion-collapse':true, 'collapse':true, show: expandedElement === 'mining' }">
           AAAAAAAAAA
         </div>
       </div>
       <div class="accordion-item">
-        <h3 class="accordion-header" id="info_title_utxo">
+        <h3 class="accordion-header" id="info_title_utxo" v-on:click="expandedElement='utxo'">
           <div class="accordion-button collapsed">
             UTXO info
           </div>
         </h3>
-        <div class="accordion-collapse collapse">
+        <div v-bind:class="{ 'accordion-collapse':true, 'collapse':true, show: expandedElement === 'utxo' }">
           AAAAAAAAAA
         </div>
       </div>
@@ -81,26 +108,39 @@
 </template>
 
 <script>
-import $ from "jquery";
 
 export default {
   name: "RPC-Dashboard",
   data() {
-    return {};
+    return {
+      RPC_URL: "http://localhost:18332/",
+      RPC_USERNAME: "root",
+      RPC_PASSWORD: "bitcoin",
+
+      expandedElement: "blockchain",
+    };
   },
   mounted() {
-    
-    $("#infoOptions .accordion-button").on("click", (ev) => {
-      const target = ev.target;
-      $("#infoOptions .accordion-collapse").removeClass("show");
-      $(target)
-        .parent()
-        .parent()
-        .find(".accordion-collapse")
-        .addClass("show");
-    });
+    //
   },
   props: {},
+  computed: {
+    RPC_FULL_URL() {
+      const parsedUrl = new URL(this.RPC_URL);
+      const auth = !this.RPC_USERNAME
+        ? ""
+        : !this.RPC_PASSWORD
+        ? `${this.RPC_USERNAME}@`
+        : `${this.RPC_USERNAME}:${this.RPC_PASSWORD}@`;
+      return `${parsedUrl.protocol}//${auth}${parsedUrl.host}${parsedUrl.pathname}`;
+    },
+  },
+  watch: {
+    //
+  },
+  methods: {
+    //
+  },
   components: {},
 };
 </script>
@@ -117,7 +157,7 @@ export default {
 }
 .action-item .action-content {
   border: 2px solid black;
-  border-top:none;
+  border-top: none;
   margin-top: 0;
   padding: 0.4rem;
 }
